032f634c205d9156a49851da16a0e819d0a8feb0
kiko <kikogdp@hotmail.com>
2024-08-29 21:29:16 -0300

    implementação do código de entrada de dados

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: conhecer e importar a classe scanner
+
+        //exibir as mensagens para nosso usuário
+
+        //obter pela classe scanner os valores do terminal
+
+        // Exibir a mensagem da conta criada
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da sua agência!");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, digite a sua agência!");
+        String agencia = sc.next();
+
+        System.out.println("Por favor, digite seu nome!");
+        String nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o saldo!");
+        double saldo = sc.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " +agencia+ ", conta "+ numero+" e seu saldo "+saldo
+        +" já está disponível para saque");
+
+
+
+
+
+        
+
+
+
+
+
+
     }
 }
